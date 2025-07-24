07c66a8994e04fb412f1b44d8cbdae9431d3a328
V.BalaAdithya <vacchaadithya@gmail.com>
2025-07-24 21:16:52 +0530

    script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,15 +1,18 @@
-
-
-    document.addEventListener('DOMContentLoaded', async() => {
+ let todos = [];
+ document.addEventListener('DOMContentLoaded', async() => {
         const data=await fetch('http://localhost:3000',{
             method: 'GET',
             headers: {'Content-Type': 'application/json'}
         })
         const result = await data.json();
         console.log(result);
+        result.map((item) => {
+            todos.push({ ID: item.ID, text: item.demo_list, editing:false
+        });
+        return todos;
+      });
+      renderTodos();
     });
-
-    let todos = [];
     function renderTodos() {
       const list = document.getElementById('todo-list');
       list.innerHTML = '';
@@ -18,9 +21,9 @@
         li.className = 'todo-item';
         if (todo.editing) {
           li.innerHTML = `
-            <input class="form-control edit-input" type="text" value="${todo.text}" onkeyup="updateTodo(event, ${index})" />
+            <input class="form-control edit-input"  type="text" value="${todo.text}" id="edit-input-${index}" onkeyup="updateTodo(event, ${index})" />
             <div class="todo-actions">
-              <button class="btn btn-success btn-sm" onclick="saveTodo(${index})">Save</button>
+              <button class="btn btn-success btn-sm" onclick="saveTodo(${todo.ID},document.getElementById('edit-input-${index}').value)">Save</button>
             </div>
           `;
         } else {
@@ -28,7 +31,7 @@
             <span>${todo.text}</span>
             <div class="todo-actions">
               <button class="btn btn-sm btn-warning" onclick="editTodo(${index})">Edit</button>
-              <button class="btn btn-sm btn-danger" onclick="deleteTodo(${index})">Delete</button>
+              <button class="btn btn-sm btn-danger" onclick="deleteTodo(${todo.ID})">Delete</button>
             </div>
           `;
         }
@@ -36,18 +39,23 @@
       });
     }
 
-    function addTodo() {
+    async function addTodo() {
         const input = document.getElementById('todo-input');
         const text = input.value;
         console.log(text);
-        fetch('http://localhost:3000/sample', {
+        await fetch('http://localhost:3000/sample', {
           method: 'POST',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({ text })
         })
     }
 
-    function deleteTodo(index) {
+    function deleteTodo(ID) {
+      console.log(ID);
+      fetch('http://localhost:3000/todo-delete', {
+        method: 'DELETE',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({ Id: ID }) })
     }
 
     function editTodo(index) {
@@ -55,6 +63,12 @@
       renderTodos();
     }
 
-    function saveTodo(index) {
+    function saveTodo(ID,demo_list) {
+      console.log(ID, demo_list);
+      fetch('http://localhost:3000/todo-update', {
+        method: 'PUT',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({ Id: ID, text: demo_list})
+      })
       renderTodos();
     }
